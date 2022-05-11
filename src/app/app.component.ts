13255917aa8656abc1fa948f99889b65519d4338
Mateo Vasquez <mateovz536@gmail.com>
2022-05-11 19:48:25 +0200

    cierra las suscripciones en app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Event, NavigationEnd, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 import { AuthService } from './pages/auth/auth.service';
 import { Role } from './shared/models/role.interface';
 
@@ -8,32 +9,46 @@ import { Role } from './shared/models/role.interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy{
 
   title:string = 'Punto de venta';
   currentRoute:string;
   isLogged: boolean = false;
   hasRoles!: Array<Role>;
   isActiveSidenav!:boolean;
+  private destroy$ = new Subject<any>();
 
   constructor(
     private authService: AuthService,
     private router: Router
   ){
     this.currentRoute = '/';
-    this.router.events.subscribe((e: Event) => {
-      if(e instanceof NavigationEnd){
-        this.currentRoute = e.url;
-      }
-    })
   }
 
   ngOnInit(): void {
-    this.authService.isLogged.subscribe(
-      (res) => (this.isLogged = res)
-    )
-    this.authService.hasRoles.subscribe(
-      (res) => (this.hasRoles = res)
-    )
+    this.authService.isLogged
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (res) => (this.isLogged = res)
+      );
+
+    this.authService.hasRoles
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (res) => (this.hasRoles = res)
+      );
+
+    this.router.events
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((e: Event) => {
+        if(e instanceof NavigationEnd){
+          this.currentRoute = e.url;
+        }
+      });
+  }
+
+  ngOnDestroy():void{
+    this.destroy$.next({});
+    this.destroy$.complete();
   }
 }
